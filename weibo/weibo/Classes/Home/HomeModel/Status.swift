c7fc306e2db08d17f27d1c7d74f7d7ed20d40cce
wangkai101 <645182907@qq.com>
2019-07-16 18:15:48 +0800

    微博来源的处理
    将var source : String?  添加属性检测器，把微博来源字符串进行截取
    var source : String? {                     // 微博来源
            didSet {
                //nil值校验
                guard let source = source, source != "" else {
                    return
                }
    
                //对来源的字符串进行处理
                   //获取起始位置和截取的长度
                let startIndex = (source as NSString).range(of: ">").location + 1
                let length = (source as NSString).range(of: "</").location - startIndex
    
                   //截取字符串
                sourceText = (source as NSString).substring(with: NSRange(location: startIndex, length: length))
            }
        }

--- weibo/weibo/Classes/Home/HomeModel/Status.swift
+++ weibo/weibo/Classes/Home/HomeModel/Status.swift
@@ -11,9 +11,27 @@ import UIKit
 @objcMembers class Status: NSObject {
     //MARK:-属性
     var created_at : String?                   // 微博创建时间
-    var source : String?                       // 微博来源
+    var source : String? {                     // 微博来源
+        didSet {
+            //nil值校验
+            guard let source = source, source != "" else {
+                return
+            }
+            
+            //对来源的字符串进行处理
+               //获取起始位置和截取的长度
+            let startIndex = (source as NSString).range(of: ">").location + 1
+            let length = (source as NSString).range(of: "</").location - startIndex
+            
+               //截取字符串
+            sourceText = (source as NSString).substring(with: NSRange(location: startIndex, length: length))
+        }
+    }
     var text : String?                         // 微博正文
     var mid : Int = 0                          // 微博id
+    
+    //MARK:- 对数据处理的属性
+    var sourceText : String?
     
     //MARK:- 自定义构造函数
     init(dict : [String : Any]) {
