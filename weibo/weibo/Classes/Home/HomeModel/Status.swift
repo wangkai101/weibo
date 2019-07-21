dcca0022e89ce4f4bc21b2287acf82848b936a68
wangkai101 <645182907@qq.com>
2019-07-21 16:41:26 +0800

    获取转发数据

--- weibo/weibo/Classes/Home/HomeModel/Status.swift
+++ weibo/weibo/Classes/Home/HomeModel/Status.swift
@@ -15,8 +15,9 @@ import UIKit
     var text : String?                         // 微博正文
     var mid : Int = 0                          // 微博id
     var user : Users?                          //微博对应的用户
-    var pic_urls : [[String : String]]?         //微博的配图
-      
+    var pic_urls : [[String : String]]?        //微博的配图
+    var retweeted_status : Status?             //对应转发的微博
+    
     
     
     
@@ -30,6 +31,11 @@ import UIKit
         //将用户字典转成用户模型对象
         if let userDict = dict["user"] as? [String : Any] {
             user = Users(dict: userDict)
+        }
+        
+        //将转发微博字典转成转发微博的模型对象
+        if let retweetedStatusDict = dict["retweeted_status"] as? [String : Any] {
+            retweeted_status = Status(dict: retweetedStatusDict)
         }
     }
     override func setValue(_ value: Any?, forUndefinedKey key: String) {}
